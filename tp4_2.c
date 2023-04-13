550499a6de15ca8821ce57a5c15cb89236b4264a
Jorge Donaire <jorgedonaire93@gmail.com>
2023-04-13 19:46:05 -0300

    hasta la parte 3 del punto 2

---
+++ tp4_2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct{
+int TareaID; //Numerado en ciclo iterativo
+char *Descripcion;  
+int Duracion; // entre 10 – 100
+}typedef Tarea;
+
+
+int main()
+{
+    srand(time(NULL));
+    int nTareas;
+    Tarea ** tareas,tareasR,tareasP;
+    puts("ingrese la cantidad de tareas a ralizar:");
+    scanf("%d",&nTareas);
+    tareas = malloc(sizeof(Tarea*)*nTareas);
+    for (int i = 0; i < nTareas; i++)
+    {
+        tareas[i] = malloc(sizeof(Tarea));
+        tareas[i]->TareaID = i+1;
+        printf("ingrese la descripcion de la tarea:\n");
+        tareas[i]->Descripcion = malloc(sizeof(char)*60);
+        scanf("%s",tareas[i]->Descripcion);
+        tareas[i]->Duracion = rand() % 91 + 10;
+
+
+    }
+    
+    for (int i = 0; i < nTareas; i++)
+    {
+        printf("id: %d\n",tareas[i]->TareaID);
+        printf("descripcion: %s\n",tareas[i]->Descripcion);
+        printf("duracion: %d\n",tareas[i]->Duracion);
+
+
+    }
+
+    
+
+
+
+
+    for (int i = 0; i < nTareas; i++)
+    {
+        free(tareas[i]->Descripcion);
+        free(tareas[i]); 
+    }
+    free(tareas);
+    return 0;
+}
