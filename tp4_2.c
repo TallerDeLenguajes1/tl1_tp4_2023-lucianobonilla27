61491c67a26825521a11490fe859ce4e811637f7
Luciano <lucianobonilla27@gmail.com>
2023-04-14 23:03:05 -0300

    commit de la funcion buscar tarea con id

--- tp4_2.c
+++ tp4_2.c
@@ -8,11 +8,13 @@ char *Descripcion;
 int Duracion; // entre 10 – 100
 }typedef Tarea;
 
+Tarea BuscarTarea(Tarea **tareas,int nTareas,int TareaID);
 
 int main()
 {
     srand(time(NULL));
-    int nTareas,menu,nTareasR=0;
+    int nTareas,menu,nTareasR=0,tareaid;
+    Tarea buscada;
     Tarea ** tareas;
     Tarea ** tareasR;
     puts("ingrese la cantidad de tareas a ralizar:");
@@ -71,6 +73,13 @@ int main()
     }
     fflush(stdin);
    
+   puts("ingrese el id de la tarea realizada que desea buscar:");
+   scanf("%d",&tareaid);
+
+   buscada = BuscarTarea(tareasR,nTareasR,tareaid);
+   printf("\n\nTarea id: %d",tareaid);
+   printf("\tdescripcion: %s\n", buscada.Descripcion);
+   printf("\tduracion: %d\n", buscada.Duracion);
 
 
     for (int i = 0; i < nTareas; i++)
@@ -83,4 +92,18 @@ int main()
     free(tareas);
     free(tareasR);
     return 0;
+}
+
+Tarea BuscarTarea(Tarea **tareas,int nTareas,int TareaID){
+     Tarea tarea_no_encontrada = {0, "Tarea no encontrada", 0};
+     for (int i = 0; i < nTareas; i++)
+     {
+        if (tareas[i]->TareaID == TareaID)
+        {
+            return *(tareas[i]);
+        }
+        
+     }
+     printf("No se encontro la tarea de id= %d",TareaID);
+     return tarea_no_encontrada;
 }
