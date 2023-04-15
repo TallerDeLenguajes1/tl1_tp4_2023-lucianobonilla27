d73fec5c68d1e2c445e6ba9e3dfc975d661688c2
Luciano <lucianobonilla27@gmail.com>
2023-04-14 22:38:06 -0300

    commit realizado antes de migrar por distintas branch

--- tp4_2.c
+++ tp4_2.c
@@ -12,10 +12,9 @@ int Duracion; // entre 10 – 100
 int main()
 {
     srand(time(NULL));
-    int nTareas,menu;
+    int nTareas,menu,nTareasR=0;
     Tarea ** tareas;
     Tarea ** tareasR;
-    Tarea ** tareasP;
     puts("ingrese la cantidad de tareas a ralizar:");
     scanf("%d",&nTareas);
 
@@ -30,48 +29,58 @@ int main()
         tareas[i] = malloc(sizeof(Tarea));
         tareas[i]->TareaID = i+1;
         printf("ingrese la descripcion de la tarea:\n");
+        fflush(stdin);
         tareas[i]->Descripcion = malloc(sizeof(char)*60);
-        scanf("%s",tareas[i]->Descripcion);
+        gets(tareas[i]->Descripcion);
         tareas[i]->Duracion = rand() % 91 + 10;
         tareasR[i] = NULL;
+        fflush(stdin);
     }
       
-    
+    printf("\n\n");
 
     //listar tareas
-    for (int i = 0; i < nTareas; i++)
-    {
-
-        printf("id: %d\n",tareas[i]->TareaID);
-        printf("descripcion: %s\n",tareas[i]->Descripcion);
-        printf("duracion: %d\n",tareas[i]->Duracion);
-        puts("Seleccione 1 si la tarea fue realizada sino seleccione 0");
+     for (int i = 0; i < nTareas; i++) {
+        printf("Tarea id: %d\n", tareas[i]->TareaID);
+        printf("\tdescripcion: %s\n", tareas[i]->Descripcion);
+        printf("\tduracion: %d\n", tareas[i]->Duracion);
+        puts("Seleccione 1 si la tarea fue realizada, sino seleccione 0");
         fflush(stdin);
-        scanf("%d",&menu);
-        if (menu == 1)
-        {
-            tareasR[i] = tareas[i];
+        scanf("%d", &menu);
+        if (menu == 1) {
+            tareasR[nTareasR] = tareas[i];
+            tareas[i] = NULL;
+            nTareasR++;
         } 
     }
 
-    puts("tareas realizadas: ");
-    for (int i = 0; tareasR[i] != NULL; i++)
-    {
-        printf("id: %d\n",tareasR[i]->TareaID);
-        printf("descripcion: %s\n",tareasR[i]->Descripcion);
-        printf("duracion: %d\n",tareasR[i]->Duracion);
-        
+    puts("/////tareas realizadas////////");
+    for (int i = 0; i < nTareasR; i++) {
+        printf("id: %d\n", tareasR[i]->TareaID);
+        printf("\tdescripcion: %s\n", tareasR[i]->Descripcion);
+        printf("\tduracion: %d\n", tareasR[i]->Duracion);
+    }
+   
+    puts("/////tareas pendientes////////");
+    for (int i = 0; i < nTareas; i++) {
+        if (tareas[i] != NULL) {
+            printf("id: %d\n", tareas[i]->TareaID);
+            printf("\tdescripcion: %s\n", tareas[i]->Descripcion);
+            printf("\tduracion: %d\n", tareas[i]->Duracion);
+        }
     }
     fflush(stdin);
    
-
 
 
     for (int i = 0; i < nTareas; i++)
     {
         free(tareas[i]->Descripcion);
         free(tareas[i]); 
+        free(tareasR[i]->Descripcion);
+        free(tareasR[i]); 
     }
     free(tareas);
+    free(tareasR);
     return 0;
 }
